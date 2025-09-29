3b5e6cad245e43ac31c9b4b8c82aef87c7624876
ocjeong <jwc9202@gmail.com>
2025-09-30 00:16:53 +0900

    module test

---
+++ module.py
@@ -0,0 +1,9 @@
+class module:
+    """A simple example class"""
+    def __init__(self):    
+        self.data = []
+    
+    i = 12345
+    
+    def f(self):
+        return "hello world"       
