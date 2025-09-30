5b5f89d8f50c15be7f690dbcff14b282b6e40d58
ocjeong <jwc9202@gmail.com>
2025-09-30 17:04:29 +0900

    modify the module class code

--- module.py
+++ module.py
@@ -6,11 +6,11 @@ print(load_messege)
 class module:
     """A simple example class"""
 
+    class_messege = "A simple example class loaded"
+    
     def __init__(self):    
         self.data = []
-    
-    class_messege = "A simple example class loaded"
-    # print(class_messege)
+        print(class_messege)
 
     j = 54_321
     i = 12_345
